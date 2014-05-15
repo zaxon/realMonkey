8cbf7fa8b1fb2b3924b226e6647f99beac66e651
zaxon <jiankangxin@gmail.com>
2014-05-15 12:00:59 +0800

    add .gitignore

--- realMonkey/test.py
+++ realMonkey/test.py
@@ -5,11 +5,11 @@ sys.path.append(r'E:\realMonkey')
 from automatormonkey.monkeyrunnercore.MonkeyRunner import rMonkeyRunner
 from automatormonkey.monkeyrunnercore.info.Enum import *
 #
-deviceName='2','c0e17bc7','1','c0e17bc7'
+deviceName='1'
 device=rMonkeyRunner(__file__,deviceName)
 #
 #FLAG.SCREENSHOT = False
-device.startActivity('com.dragon.android.pandaspace/com.dragon.android.pandaspace.guid.GuidOneKeyActivity')
+device.startActivity('com.dragon.android.pandaspace/.main.MainActivity')
 #device.click(UIELEMENT.TEXT,'短信')
 #device.press('KEYCODE_HOME')
 #device.click(UIELEMENT.TEXT,'百度')
